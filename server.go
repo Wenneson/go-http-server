018ea831584d6917b7e0f600c64e33c3a1182386
MWenneson <michael.l.wenneson@gmail.com>
2024-04-24 21:48:42 -0500

    update

--- server.go
+++ server.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-    // Define the folder to serve the static files from
-    fs := http.FileServer(http.Dir("./public"))
+	// Define the folder to serve the static files from
+	fs := http.FileServer(http.Dir("./public"))
 
-    // Handle all requests by serving a file of the same name
-    http.Handle("/", fs)
+	// Handle all requests by serving a file of the same name
+	http.Handle("/", fs)
 
-    // Define the address to listen on, use "" to listen on all IP addresses of the machine
-    addr := ":3000" // Listening on all interfaces at port 3000
+	// Define the address to listen on, use "" to listen on all IP addresses of the machine
+	addr := ":3000" // Listening on all interfaces at port 3000
 
-    // Print the address and start the server
-    log.Printf("Listening on http://%s\n", addr)
-    err := http.ListenAndServe(addr, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Print the address and start the server
+	log.Printf("Listening on http://%s\n", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
